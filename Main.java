f4c5c5903e1fa5abf1d54fd318455bc672c2f0e4
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-13 14:02:02 +0000

    asked the user for a number and then found every number leading up to the user number that divides without a remainder

--- Main.java
+++ Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program the finds out the numbers that divide without a remainder from a user entered number
+ * @author Daniel Rahmani
  */
 public class Main {
+
+  public static void factors(int n){
+    for(int i = 1; i <= n; i++){
+      if(n % i == 0){
+        System.out.println(i);
+      }
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // tell the user the numbers that divide with no remainder
+    System.out.println("Please enter a number to find the factors of");
+    int n = input.nextInt();
+    factors(n);
     
   }
 }
